3afacff33fe6c1baa6f5964b92185df6212146a1
Tomicarrie <103190897+Tomicarrie@users.noreply.github.com>
2025-06-13 21:09:57 -0300

    Update UnoController.java

--- TP4/unoback/src/main/java/org/udesa/unoback/controller/UnoController.java
+++ TP4/unoback/src/main/java/org/udesa/unoback/controller/UnoController.java
@@ -11,29 +11,10 @@ import org.udesa.unoback.service.UnoService;
 import java.util.List;
 import java.util.UUID;
 
-
-// controller nos dice mapeo esto a una pagina web. Quiero escribir/devolver una pagina web
-// 2 tipos de controler
-// - REST Controller: para hacer aplicaciones REST full. Es mas simpke
-// - Controller: este gestiona HTML
-
-
-
 @Controller
 public class UnoController {
-    //@GetMapping("/") // le mapeo a la raiz de la pagina web este mensaje, en el localhost 8080
-    //public String saludo() { return "index"; }
+
     @Autowired UnoService unoService;
-
-
-    @GetMapping("/hola")
-    public ResponseEntity<String> holaMundo() {
-        return new ResponseEntity<>("Respuesta a hola mundo", HttpStatus.OK);
-    }
-
-    //@PostMapping ("newmatch") public ResponseEntity newMatch(@RequestParam List<String> players) {
-    //    return ResponseEntity.ok(UUID.randomUUID());
-    //}
 
     @PostMapping ("newmatch") public ResponseEntity newMatch(@RequestParam List<String> players) {
         return ResponseEntity.ok(unoService.newMatch(players));
@@ -54,9 +35,6 @@ public class UnoController {
     public ResponseEntity playerHand(@PathVariable UUID matchId) {
         return ResponseEntity.ok( unoService.playerHand( matchId ).stream().map( each -> each.asJson()));
     }
-
-    // Datos inválidos, parámetros mal formados, incompletos o incorrectos... podría ser un internal error o runtime, algo de bajo nivel
-    // Jugada invalida, jugador inexistente, carta incorrecta, turno incorrecto... podría ser un ilegal argument, algo a nivel de negocio.
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegal(IllegalArgumentException exception) {
